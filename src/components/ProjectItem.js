1a9fd8304db47d163a6d29529bd830175b3f33d9
lorna gathumbi <nyawiralornah@gmail.com>
2024-10-30 14:11:36 +0300

    react lists and keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+      {technologies.map((tech, index) => (
+          <span key={index} className="technology">
+            {tech}
+          </span>
+        ))}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
